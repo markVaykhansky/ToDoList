f0d1297ceccff47d35b03f6ce09666a8107e4423
MarkVaykhansky <prhing@gmail.com>
2022-11-27 17:44:41 +0200

    WIP

--- src/Authenticator.jsx
+++ src/Authenticator.jsx
@@ -3,33 +3,50 @@ import { Link, Navigate } from 'react-router-dom';
 import { useContext } from "react";
 import { UserContext } from "./userContext";
 
+async function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+const userToPassword = {
+    'Mark': '1234',
+    'Mark2': '5678'
+}
 
 export function Authenticator({ redirectTo }) {
     const { user, onUserChanged } = useContext(UserContext);
 
-    const [inputValue, setInputValue] = useState('');
-    
-    const onInputChangeMethod = (eventArgs) => {
-        const currentInput = eventArgs.target.value;
-        setInputValue(currentInput);
-      }
+    const [userInputValue, setUserInputValue] = useState('');
+    const [passwordInputValue, setPasswordInputValue] = useState('');
+    const [loggingIn, setLoggingIn] = useState(false);
+    const [errorMessage, setErrorMessage] = useState('');
 
-    const onLogInButtonClicked = () => {
-        onUserChanged(inputValue);
-    }
+    if(user) return (<Navigate to={redirectTo} />);
 
-    if(user) {
-        return (<Navigate to={redirectTo} />);
+    const onUserInputChange = (eventArgs) => setUserInputValue(eventArgs.target.value)
+    const onPasswordInputChange = (eventArgs) => setPasswordInputValue(eventArgs.target.value)
+
+    const onLogInButtonClicked = async () => {
+        setLoggingIn(true);
+        sleep(3000);
+
+        if(passwordInputValue === '') {
+            setErrorMessage('Empty password')
+        }
+
+        if(password)
+
+        onUserChanged(userInputValue);
+
     }
 
     return (
-    <div>
-        <span>Please Log In </span>
-        <input value={inputValue} onChange={onInputChangeMethod} />
-        <button onClick={onLogInButtonClicked}>
-            <Link to={'/taskList'}>
-                Log In
-            </Link>
-        </button>
+    <div style={{ paddingLeft: '50px' }}>
+        <h1>Please Log In </h1>
+        {errorMessage && <h2 style={{color: 'red'}}>{'Error: ' + errorMessage}</h2>}
+        <div>User name: </div>
+        <input value={userInputValue} onChange={onUserInputChange} />
+        <div>Password</div>
+        <input type={'password'} value={passwordInputValue} onChange={onPasswordInputChange} />
+        <button onClick={onLogInButtonClicked}>{loggingIn ? 'Logging in...' :  'Log In'}</button>
     </div>)
 }
